3f19d5ca082e4605bf12d1fb77b026c4c0ced303
kitamatsu junpei <umiwashi-teilu-z@ezweb.ne.jp>
2021-09-18 21:53:35 +0900

    Userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'aa0000'}
+    password_confirmation {password}
+    family_name           {'あア木'}
+    first_name            {'あア木'}
+    family_name_kana      {'キ'}
+    first_name_kana       {'キ'}
+    birthdays             {'1930-3-13'}
+  end
+end
